f233b9d6d74549b1e5471c29dbce8b1be5e8f08b
yunhan0 <yunhn.lee@gmail.com>
2017-03-17 13:56:15 +1100

    initialize the project

---
+++ Assets/Scripts/CameraCalibration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCalibration : MonoBehaviour {
+
+	public Transform player;
+	private float offset = -2.0f;
+	private bool startCalibration = false;
+
+	void Update() {
+		if (startCalibration)
+			return;
+		if (player.position.y <= transform.position.y)
+			startCalibration = true;
+	}
+
+	void LateUpdate() {
+		if (startCalibration) {
+			transform.position = new Vector3 (0.0f, player.transform.position.y + offset, -10.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/LineCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour {
+	public GameObject linePrefab;
+
+	Line activeLine;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject lineGO = Instantiate (linePrefab);
+			activeLine = lineGO.GetComponent<Line> ();
+		}	
+
+		if (Input.GetMouseButtonUp (0)) {
+			activeLine = null;
+		}
+
+		if (activeLine != null) {
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			activeLine.UpdateLine (mousePos);
+		}
+	}
+}
