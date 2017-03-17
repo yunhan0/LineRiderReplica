05699a250dd49ba9876221121e9177a45dcf59b0
yunhan0 <yunhn.lee@gmail.com>
2017-03-17 14:27:20 +1100

    add game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject player;
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = player.GetComponent<Rigidbody2D> ();
+	}
+
+	public void MovePlayer() {
+		rb.bodyType = RigidbodyType2D.Dynamic;
+	}
+
+	public void StopPlayer() {
+		rb.bodyType = RigidbodyType2D.Static;
+	}
+}
